ce8d788ac3370b3b4335c795db55fc41444b7325
musasazib <musasazib@gmail.com>
2021-09-16 11:19:06 +0600

    setTimeout simple Asynchronous JS using

---
+++ timeout.js
@@ -0,0 +1,11 @@
+console.log('I am the first person');
+console.log('I am the secund person');
+setTimeout(function () {
+    console.log('I am using VS code');
+}, 5000)
+setTimeout(() => {
+    console.log('Exploring Set-timeout');
+}, 4000)
+setTimeout(() => console.log('i am good boy'));
+console.log('I am the third person');
+console.log('I am the fourth person');
